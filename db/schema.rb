29aba35b094981d4640f657eb98badcf1b8935fa
Julie Menge <juliemenge@gmail.com>
2018-08-12 15:45:19 -0400

    static pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_12_181435) do
+ActiveRecord::Schema.define(version: 2018_08_12_193053) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
